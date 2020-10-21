4c9a6747f75a52c6e21bf88875654b6f0b0cc5b0
vendetta <okta.pilopa@staff.itera.ac.id>
2020-10-21 15:47:19 +0700

    up

--- application/views/monitoring/index.php
+++ application/views/monitoring/index.php
@@ -5,7 +5,7 @@
         <!-- <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="./">Home</a></li>
             <li class="breadcrumb-item">Pages</li>
-            <li class="breadcrumb-item active" aria-current="page">Blank Page</li>
+            <li class="breadcrumb-item active" aria-current="page">Blank Page</li> 
         </ol> -->
     </div>
 
