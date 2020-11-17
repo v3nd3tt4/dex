affa9052caaeb640753eb22fa73f40db8f8d337d
v3nd3tt4 <oktapilopa@gmail.com>
2020-11-17 22:28:22 +0700

    up

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -13,29 +13,29 @@ class Home extends CI_Controller
 
     public function index()
     {
-       	$q = $this->db->query("SELECT * FROM dryer
-		   WHERE humidity NOT LIKE '%t%' AND humidity NOT LIKE '%n%' AND humidity != '' AND temp NOT LIKE '%t%' AND temp NOT LIKE '%n%' AND temp != '' ");
-		$data['row'] = $q;
+       	// $q = $this->db->query("SELECT * FROM dryer
+		//    WHERE humidity NOT LIKE '%t%' AND humidity NOT LIKE '%n%' AND humidity != '' AND temp NOT LIKE '%t%' AND temp NOT LIKE '%n%' AND temp != '' ");
+		// $data['row'] = $q;
 
-		$q2 = $this->db->query("SELECT * FROM dryer2
-		   WHERE humidity NOT LIKE '%t%' AND humidity NOT LIKE '%n%' AND humidity != '' AND temp NOT LIKE '%t%' AND temp NOT LIKE '%n%' AND temp != '' ");
-		$data['row2'] = $q2;
+		// $q2 = $this->db->query("SELECT * FROM dryer2
+		//    WHERE humidity NOT LIKE '%t%' AND humidity NOT LIKE '%n%' AND humidity != '' AND temp NOT LIKE '%t%' AND temp NOT LIKE '%n%' AND temp != '' ");
+		// $data['row2'] = $q2;
 
-		$q3 = $this->db->query("SELECT * FROM dryer3
-		   WHERE humidity NOT LIKE '%t%' AND humidity NOT LIKE '%n%' AND humidity != '' AND temp NOT LIKE '%t%' AND temp NOT LIKE '%n%' AND temp != '' ");
-		$data['row3'] = $q3;
+		// $q3 = $this->db->query("SELECT * FROM dryer3
+		//    WHERE humidity NOT LIKE '%t%' AND humidity NOT LIKE '%n%' AND humidity != '' AND temp NOT LIKE '%t%' AND temp NOT LIKE '%n%' AND temp != '' ");
+		// $data['row3'] = $q3;
 
-		$q4 = $this->db->query("SELECT * FROM dryer4
-		   WHERE humidity NOT LIKE '%t%' AND humidity NOT LIKE '%n%' AND humidity != '' AND temp NOT LIKE '%t%' AND temp NOT LIKE '%n%' AND temp != '' ");
-		$data['row4'] = $q4;
+		// $q4 = $this->db->query("SELECT * FROM dryer4
+		//    WHERE humidity NOT LIKE '%t%' AND humidity NOT LIKE '%n%' AND humidity != '' AND temp NOT LIKE '%t%' AND temp NOT LIKE '%n%' AND temp != '' ");
+		// $data['row4'] = $q4;
 
-		$q5 = $this->db->query("SELECT * FROM dryer5
-		   WHERE humidity NOT LIKE '%t%' AND humidity NOT LIKE '%n%' AND humidity != '' AND temp NOT LIKE '%t%' AND temp NOT LIKE '%n%' AND temp != '' ");
-		$data['row5'] = $q5;
+		// $q5 = $this->db->query("SELECT * FROM dryer5
+		//    WHERE humidity NOT LIKE '%t%' AND humidity NOT LIKE '%n%' AND humidity != '' AND temp NOT LIKE '%t%' AND temp NOT LIKE '%n%' AND temp != '' ");
+		// $data['row5'] = $q5;
 
-		$q6 = $this->db->query("SELECT * FROM dryer6
-		   WHERE humidity NOT LIKE '%t%' AND humidity NOT LIKE '%n%' AND humidity != '' AND temp NOT LIKE '%t%' AND temp NOT LIKE '%n%' AND temp != '' ");
-		$data['row6'] = $q6;
+		// $q6 = $this->db->query("SELECT * FROM dryer6
+		//    WHERE humidity NOT LIKE '%t%' AND humidity NOT LIKE '%n%' AND humidity != '' AND temp NOT LIKE '%t%' AND temp NOT LIKE '%n%' AND temp != '' ");
+		// $data['row6'] = $q6;
 
         $data['title'] = 'Dashboard';
         $this->load->view('_layout_sifa/header', $data);
